75614424ef318889dc6595d33e28d1682a958703
Weiden <weidencore@gmail.com>
2021-07-27 12:25:20 -0300

    Incluido tratamentos erro e tipos consumo

--- WebAPI/WebAPI/Controllers/ClienteController.cs
+++ WebAPI/WebAPI/Controllers/ClienteController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -21,12 +22,27 @@ namespace WebAPI.Controllers
         }
 
         [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]  //Define tipo de dados para consumir API.
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
         public ActionResult CriarCliente(ClienteDto clienteDto)
         {
-        var lista = new List<ClienteDto>();
-        lista.Add(clienteDto);
-        return CreatedAtAction(nameof(CriarCliente), Guid.NewGuid());
+            try
+            {
+                //var lista = new List<ClienteDto>();
+                //throw new Exception("Erro ao salvar no banco de dados!");
+                //lista.Add(clienteDto);
+                //return CreatedAtAction(nameof(CriarCliente), Guid.NewGuid());
 
+                return BadRequest();
+                
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, "Erro");
+            }
+            
         }
 
     }
--- WebAPI/WebAPI/Controllers/ClienteDto.cs
+++ WebAPI/WebAPI/Controllers/ClienteDto.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace WebAPI.Controllers
 {
     public class ClienteDto
     {
+        [Required]  //Para tratar obrigatoriedade de campos.
         public string Nome { get; set; }
+        public string Cpf { get; set;  }
+        //public int  Idade { get; set; }
      }
 }
