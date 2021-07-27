5452baf5d199afdb19d6fe0c73a5279866c5d7e5
Weiden <weidencore@gmail.com>
2021-07-27 11:25:50 -0300

    Commit inicial - Teste API

---
+++ WebAPI/WebAPI/Controllers/ClienteController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route(template: "api/[controller]")]
+    [ApiController]
+    public class ClienteController : Controller
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<ClienteDto>> ObterClientes() 
+        {
+            var lista = new List<ClienteDto>();
+            lista.Add(item: new ClienteDto() { Nome = "Cristiano"});
+            lista.Add(item: new ClienteDto() { Nome = "Weiden" });
+            return Ok(lista);
+        }
+
+        [HttpPost]
+        public ActionResult CriarCliente(ClienteDto clienteDto)
+        {
+        var lista = new List<ClienteDto>();
+        lista.Add(clienteDto);
+        return CreatedAtAction(nameof(CriarCliente), Guid.NewGuid());
+
+        }
+
+    }
+}
---
+++ WebAPI/WebAPI/Controllers/ClienteDto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    public class ClienteDto
+    {
+        public string Nome { get; set; }
+     }
+}
